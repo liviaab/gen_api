b929fa9f08859f51471a33332abf3fcd09f9cbb3
liviaab <liviaab7@gmail.com>
2021-06-05 09:41:51 -0300

    add credo

--- lib/gen_api/users/user.ex
+++ lib/gen_api/users/user.ex
@@ -1,4 +1,6 @@
 defmodule GenApi.Users.User do
+  @moduledoc false
+
   use Ecto.Schema
   import Ecto.Changeset
 
--- lib/gen_api_web/telemetry.ex
+++ lib/gen_api_web/telemetry.ex
@@ -1,4 +1,6 @@
 defmodule GenApiWeb.Telemetry do
+  @moduledoc false
+
   use Supervisor
   import Telemetry.Metrics
 

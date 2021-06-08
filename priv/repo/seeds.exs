2d24da5dceec3472202ea7c94ae3c91622b5790c
liviaab <liviaab7@gmail.com>
2021-06-08 08:04:04 -0300

    Add seeds in batches

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,8 +10,15 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-alias GenApi.Users
+alias GenApi.{Repo, Users.User}
 
-Enum.map(1..1_000_000, fn _ ->
-  Users.create_user(%{points: 0})
+users = Enum.map(1..1_000_000, fn _ ->
+	now = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
+	%{points: 0, inserted_at: now, updated_at: now}
+end)
+
+list_of_batches = Enum.chunk_every(users, 21_500)
+
+Enum.each(list_of_batches, fn batch ->
+	Repo.insert_all(User, batch)
 end)

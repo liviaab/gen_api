fabad533cbf19afc72babc61a923a5186c58fe00
liviaab <liviaab7@gmail.com>
2021-06-08 08:28:01 -0300

    fix format

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,13 +12,14 @@
 
 alias GenApi.{Repo, Users.User}
 
-users = Enum.map(1..1_000_000, fn _ ->
-	now = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
-	%{points: 0, inserted_at: now, updated_at: now}
-end)
+users =
+  Enum.map(1..1_000_000, fn _ ->
+    now = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
+    %{points: 0, inserted_at: now, updated_at: now}
+  end)
 
 list_of_batches = Enum.chunk_every(users, 21_500)
 
 Enum.each(list_of_batches, fn batch ->
-	Repo.insert_all(User, batch)
+  Repo.insert_all(User, batch)
 end)
